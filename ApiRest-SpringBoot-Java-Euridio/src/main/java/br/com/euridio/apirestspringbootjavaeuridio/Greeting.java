9599bde6f83bc2e12e88fa06972a4477dcc68d8f
renzotakada <renzotakada1@gmail.com>
2023-07-20 11:10:18 -0300

    feat: initial commit

---
+++ ApiRest-SpringBoot-Java-Euridio/src/main/java/br/com/euridio/apirestspringbootjavaeuridio/Greeting.java
@@ -0,0 +1,5 @@
+package br.com.euridio.apirestspringbootjavaeuridio;
+
+public record Greeting (long id,String content){
+
+}
---
+++ ApiRest-SpringBoot-Java-Euridio/src/test/java/br/com/euridio/apirestspringbootjavaeuridio/ApiRestSpringBootJavaEuridioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.euridio.apirestspringbootjavaeuridio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestSpringBootJavaEuridioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
